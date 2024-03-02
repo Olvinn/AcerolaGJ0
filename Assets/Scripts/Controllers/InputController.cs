ac431b6dc683fabe063eb0a9d5100db935af11d4
Olvin <paradox_x94@hotmail.com>
2024-03-02 16:34:08 +0300

    upd: input controller unused var

--- Assets/Scripts/Controllers/InputController.cs
+++ Assets/Scripts/Controllers/InputController.cs
@@ -10,8 +10,6 @@ namespace Controllers
         public Vector2 lookDirection;
         public Action onInteract;
 
-        private bool _isMouse;
-
         public void OnMove(InputValue value)
         {
             move = value.Get<Vector2>();
@@ -22,7 +20,6 @@ namespace Controllers
             var pos = value.Get<Vector2>();
             pos += new Vector2(Screen.width * .5f, Screen.height * .5f);
             lookDirection = -pos;
-            _isMouse = true;
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
@@ -30,7 +27,6 @@ namespace Controllers
         public void OnLookGamepad(InputValue value)
         {
             lookDirection = value.Get<Vector2>();
-            _isMouse = false;
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
