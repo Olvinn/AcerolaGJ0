747b7c2434575a0d1763019cd2c5a605796c2559
Olvin <paradox_x94@hotmail.com>
2024-03-02 16:33:24 +0300

    fix: gamepad and mouse input

--- Assets/Scripts/Controllers/InputController.cs
+++ Assets/Scripts/Controllers/InputController.cs
@@ -10,6 +10,8 @@ namespace Controllers
         public Vector2 lookDirection;
         public Action onInteract;
 
+        private bool _isMouse;
+
         public void OnMove(InputValue value)
         {
             move = value.Get<Vector2>();
@@ -20,11 +22,17 @@ namespace Controllers
             var pos = value.Get<Vector2>();
             pos += new Vector2(Screen.width * .5f, Screen.height * .5f);
             lookDirection = -pos;
+            _isMouse = true;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
         }
         
         public void OnLookGamepad(InputValue value)
         {
             lookDirection = value.Get<Vector2>();
+            _isMouse = false;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
         }
 
         public void OnInteract(InputValue value)
