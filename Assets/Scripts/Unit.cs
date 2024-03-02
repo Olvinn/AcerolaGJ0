83cf6f997e5b1b0165415d131d97a9285f77928b
Olvin <paradox_x94@hotmail.com>
2024-03-02 22:19:23 +0300

    fix: unit rotation with gamepad

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,20 +5,28 @@ using UnityEngine.AI;
 public class Unit : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent _agent;
+    private Vector3 _cachedMovDir;
 
     public void Move(Vector3 dir)
     {
         if (dir.sqrMagnitude == 0)
             return;
+        _cachedMovDir = dir;
         _agent.Move(dir * Time.deltaTime);
     }
 
     public void Look(Vector3 dir)
     {
-        if (dir.sqrMagnitude == 0)
-            return;
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(dir),
-            Time.deltaTime * GameConfigsContainer.instance.config.playerAngularSpeed);
+        if (dir.sqrMagnitude != 0)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(dir),
+                Time.deltaTime * GameConfigsContainer.instance.config.playerAngularSpeed);
+        }
+        else if (_cachedMovDir.sqrMagnitude != 0)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(_cachedMovDir),
+                Time.deltaTime * GameConfigsContainer.instance.config.playerAngularSpeed);
+        }
     }
 
     public void Teleport(Vector3 pos, Quaternion rot)
